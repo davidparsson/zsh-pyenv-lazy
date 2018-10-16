3671972cb68e523c7cf8e7fdbd1f9fa913d5dc29
David Pärsson <david@parsson.se>
2018-10-16 12:10:17 +0200

    Properly unset function

--- pyenv-lazy.plugin.zsh
+++ pyenv-lazy.plugin.zsh
@@ -3,7 +3,7 @@ if type pyenv &> /dev/null; then
     local PYENV_SHIMS="${PYENV_ROOT:-${HOME}/.pyenv}/shims"
     export PATH="${PYENV_SHIMS}:${PATH}"
     function pyenv() {
-        unset pyenv
+        unset -f pyenv
         eval "$(command pyenv init -)"
         pyenv $@
     }
